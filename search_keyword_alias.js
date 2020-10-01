1c08da3e36bf31016f0eefcc3a07c001c27ac47f
shjanken <janken.wang@gmail.com>
2020-10-01 20:56:02 +0800

    简化了代码

--- search_keyword_alias.js
+++ search_keyword_alias.js
@@ -11,15 +11,8 @@ if (opts.length != 3) {
 }
 
 const request_alias = async () => {
-    try {
         const request_uri = encodeURI(`http://aolai.souche.com/v1/searchApi/suggestKeyword.json?keyword=${opts[2]}`)
-        const res = await axios.get(request_uri)
-        if (res.status == 200)
-            // console.log(res)
-            console.log(res.data.data.allSeries)
-    }catch (err) {
-        console.error(err)
-    }
+        return res = await axios.get(request_uri)
 }
 
-request_alias().then(console.log).catch(console.error)
+request_alias().then(res => console.log(res.data.data.allSeries)).catch(console.error)
